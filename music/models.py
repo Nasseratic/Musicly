529ed38d6d5880c9f39557b3985150afae0cacad
yaso <yaso>
2017-11-24 13:58:05 +0200

    first commit

---
+++ music/models.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.db import models
+
+# Create your models here.
+
+
+class Band(models.Model):
+    name = models.CharField(max_length=200)
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    dateOfBirth = models.DateField()
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.CharField(max_length=500)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+
+class Single_Song(models.Model):
+    name = models.CharField(max_length=250)
+    releaseDate = models.DateField()
+    genre = models.CharField(max_length=250)
+    lyrics = models.CharField(max_length=2000)
+    length = models.DecimalField(decimal_places=3,max_digits=6)
+    playlists = models.ManyToManyField(Playlist)
+    bands = models.ForeignKey(Band, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+
+
+class Single_Artist(models.Model):
+    name = models.CharField(max_length=200)
+    dateOfBirth = models.DateField()
+
+
+class Song_Album(models.Model):
+    name = models.CharField(max_length=250)
+    releaseDate = models.DateField()
+    genre = models.CharField(max_length=250)
+    lyrics = models.CharField(max_length=2000)
+    length = models.DecimalField(decimal_places=3, max_digits=6)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    playlists = models.ManyToManyField(Playlist)
+    bands = models.ForeignKey(Band, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+
