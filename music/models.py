85f0f5cb6ee90da52e9033ad48cbcc7f58311880
yasserotiefy <co2yasser@gmail.com>
2017-11-25 15:26:06 +0200

    More Model edits

--- music/models.py
+++ music/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 from django.db import models
+import json
+from collections import namedtuple
 
 # Create your models here.
 
@@ -11,14 +13,17 @@ from django.db import models
 class Band(models.Model):
     name = models.CharField(max_length=200)
 
+
 class Artist(models.Model):
     name = models.CharField(max_length=200)
     dateOfBirth = models.DateField()
     band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True,blank=True)
 
+
 class Playlist(models.Model):
     name = models.CharField(max_length=300)
     description = models.CharField(max_length=500)
+
 
 
 class Album(models.Model):
@@ -26,18 +31,18 @@ class Album(models.Model):
     band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True,blank=True)
 
 
-
 class Song(models.Model):
     name = models.CharField(max_length=250)
-    path = models.CharField(max_length=400)
+    path = models.CharField(max_length=400,null=True)
     releaseDate = models.DateField()
     genre = models.CharField(max_length=250)
     lyrics = models.CharField(max_length=2000)
     length = models.DecimalField(decimal_places=3, max_digits=6)
     album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True,blank=True)
-    playlists = models.ManyToManyField(Playlist, null=True,blank=True)
+    playlists = models.ManyToManyField(Playlist)
     bands = models.ForeignKey(Band, on_delete=models.CASCADE, null=True,blank=True)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE,null=False,blank=False)
+
 
 
 
--- music/urls.py
+++ music/urls.py
@@ -13,4 +13,16 @@ urlpatterns = [
     url(r'^artist/(?P<artist_id>[0-9]+)/$', views.get_artistById, name='get_artistById'),
     url(r'^allBands/$', views.all_bands, name='all_Bands'),
     url(r'^band/(?P<band_id>[0-9]+)/$', views.get_bandById, name='get_albumById'),
+    url(r'^delBand/(?P<band_id>[0-9]+)/$', views.delete_bandById, name='delete_bandById'),
+    url(r'^delSong/(?P<song_id>[0-9]+)/$', views.delete_songById, name='delete_songById'),
+    url(r'^delAlbum/(?P<album_id>[0-9]+)/$', views.delete_albumById, name='delete_albumById'),
+    url(r'^delArtist/(?P<artist_id>[0-9]+)/$', views.delete_artistById, name='delete_artistById'),
+    url(r'^delPlaylist/(?P<playlist_id>[0-9]+)/$', views.delete_playlistById, name='delete_playlistById'),
+    url(r'^addBand/$', views.add_band, name='add_band'),
+    url(r'^addSong/$', views.add_song, name='add_song'),
+    url(r'^addAlbum/$', views.add_album, name='add_album'),
+    url(r'^addArtist/$', views.add_artist, name='add_artist'),
+    url(r'^addPlaylist/$', views.add_playlist, name='add_playlist'),
+
+
 ]
